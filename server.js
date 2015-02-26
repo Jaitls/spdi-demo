612c8ec62b7c2ed79a7a6db2613ba338ca036b0f
Jaitls <scottjaitly@gmail.com>
2015-02-25 23:27:50 -0500

    angular controller test

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var compress = require('compression');
 var app = express();
 
+app.use(compress());
+
 app.get('/', function(req, res) {
-	res.sendFile('index.html', {root: __dirname }); // load the single view index.html main page
+	res.sendFile('index.html', {root: __dirname}); // load the single view index.html main page
 });
+
+app.use('/src', express.static(__dirname + '/src'));
 
 app.listen(8080);
 console.log("App listening on port 8080");
