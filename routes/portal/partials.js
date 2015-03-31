f5a67f79d0e1ea317fba5872e68be7dd94d5aa45
Jaitls <scottjaitly@gmail.com>
2015-03-30 23:50:02 -0400

    update partial routing

--- routes/portal/partials.js
+++ routes/portal/partials.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
 	app.get('/partials/:name', function(req, res, next) {
-		var name = req.params.name + '.html';
+		var name = req.params.name;
+		if (name.split('.').pop() !== 'html') name = name + '.html';
 		res.sendFile('/views/partials/' + name, {
 			root: './'
 		});
